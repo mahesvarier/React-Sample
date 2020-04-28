e62d5765be646e89adb14b49bebfb1275e07cf55
mahesmvp <mahesmvp94@gmail.com>
2020-04-28 18:48:58 +1000

    implementing react life cycle methods

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
+import CoursePage from "./CoursePage";
 
 function App() {
   function getPage() {
     const route = window.location.pathname;
     if (route === "/about") return <AboutPage />;
+    if (route === "/courses") return <CoursePage />;
     return <HomePage />;
   }
   return (
