341209cd58af88406ac937c3e563a50cbfbb2374
mahesmvp <mahesmvp94@gmail.com>
2020-04-28 19:50:14 +1000

    Links and NavLinks implementation

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="jumbotron">
       <h1>PluralSight Admin</h1>
       <p>React, flux and React Router for ultra-responsive web apps.</p>
-      <a href="about">About</a>
+      <Link to="about" className="btn btn-primary">
+        About
+      </Link>
+      {/* <a href="about">About</a> */}
     </div>
   );
 }
