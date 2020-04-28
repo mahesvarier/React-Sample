7135ca3fa9ee6b20f12aa035eaab190fccfd1603
mahesmvp <mahesmvp94@gmail.com>
2020-04-28 20:02:07 +1000

    404 and Switch, redirect

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,20 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursePage from "./CoursePage";
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Route path="/" exact component={HomePage}></Route>
-      <Route path="/courses" component={CoursePage}></Route>
-      <Route path="/about" component={AboutPage}></Route>
+      <Switch>
+        <Route path="/" exact component={HomePage}></Route>
+        <Route path="/courses" component={CoursePage}></Route>
+        <Route path="/about" component={AboutPage}></Route>
+        <Redirect from="/about-page" to="/about"></Redirect>
+        <Route component={NotFoundPage}></Route>
+      </Switch>
     </div>
   );
 }
