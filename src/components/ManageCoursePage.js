d6c7af958daddb2976c2757b95cc8680ac845784
mahesvarier <mahesvarier@gmail.com>
2020-04-29 16:56:18 +1000

    testing git account

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -5,7 +5,7 @@ const ManageCoursePage = (props) => {
   return (
     <>
       <h2>Manage Course</h2>
-      <Prompt when={true} message="Are you sure you want to leave/?"></Prompt>
+      <Prompt when={true} message="Are you sure you want to leave /?"></Prompt>
       {props.match.params.slug}
     </>
   );
